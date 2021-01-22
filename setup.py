f3c8b79012b5c546792bd373b98ee7df8e12e738
gjbaker <gregory_baker2@hms.harvard.edu>
2021-01-21 22:07:22 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = [
     'hurry.filesize==0.9',
 ]
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'CyLinter'
 AUTHOR = 'Gregory J. Baker'
 AUTHOR_EMAIL = 'gregory_baker2@hms.harvard.edu'
