cf32f64d4b676fcea22b4d606ede20892fe909e0
gjbaker <gregory_baker2@hms.harvard.edu>
2021-01-25 11:53:49 -0500

    Switch back to synapseclient 2.2.2.

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ requires = [
     'rpy2==3.4.2',
     'bridson==0.1.0',
     'hurry.filesize==0.9',
-    'synapseclient==2.0.0',  # v2.2.2 (current version as of 01/25/21 fails to transfer under current distribution)
+    'synapseclient==2.2.2',
 ]
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 DESCRIPTION = 'CyLinter'
 AUTHOR = 'Gregory J. Baker'
 AUTHOR_EMAIL = 'gregory_baker2@hms.harvard.edu'
