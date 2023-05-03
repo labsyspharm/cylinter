f3a5a54978467ed8badcbf09e4ba9c73012bb446
Jeremy Muhlich <jmuhlich@bitflood.org>
2023-05-03 12:48:25 -0400

    Prep for v0.0.45 release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
     'cellcutter>=0.2.6',
 ]
 
-VERSION = '0.0.44'
+VERSION = '0.0.45'
 DESCRIPTION = 'CyLinter'
 AUTHOR = 'Gregory J. Baker'
 AUTHOR_EMAIL = 'gregory_baker2@hms.harvard.edu'
